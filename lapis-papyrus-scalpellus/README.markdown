64e98216e0f7b4452fbf5516bbf1cf73943a9439
Hunter Wallen <72570987+hunterwallen@users.noreply.github.com>
2020-10-27 11:18:28 -0600

    lapis, papyrus, scalpellus initial upload

---
+++ lapis-papyrus-scalpellus/README.markdown
@@ -0,0 +1,5 @@
+# Lapis, Papyrus, Scalpellus
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hunterwallen/pen/mdERmmR](https://codepen.io/hunterwallen/pen/mdERmmR).
+
+
---
+++ lapis-papyrus-scalpellus/dist/script.js
@@ -0,0 +1,55 @@
+let objects = ['lapis', 'papyrus', 'scalpellus'];
+
+document.querySelector('#lapis').addEventListener('click', function(e) {
+  let playersChoice = 'lapis';
+  compareChoices(playersChoice);
+});
+document.querySelector('#papyrus').addEventListener('click', function(e) {
+  let playersChoice = 'papyrus';
+  compareChoices(playersChoice);
+});
+document.querySelector('#scalpellus').addEventListener('click', function(e) {
+  let playersChoice = 'scalpellus';
+  compareChoices(playersChoice);
+});
+
+
+
+
+function computerChooses() { 
+  let randomIndex = Math.floor(Math.random() * objects.length);
+  return objects[randomIndex];
+}
+
+
+function compareChoices(playersChoice) {
+  let computersChoice = computerChooses();
+  let resultStatement = "Computer chose " + computersChoice + " and player chose " + playersChoice + ".";
+  let win = "Player won! ";
+  let lose = "Player lost! ";
+  if(playersChoice === computersChoice) {
+    displayResults("It's a tie! " + resultStatement)
+  } else if (playersChoice === objects[0]) {
+    if (computersChoice === objects[2]) {
+      displayResults(win + resultStatement)
+    } else { 
+    displayResults(lose + resultStatement)
+    }
+  } else if (playersChoice === objects[1]) {
+    if (computersChoice === objects[0]) {
+      displayResults(win + resultStatement)
+    } else {
+      displayResults(lose + resultStatement)
+    }
+  } else if (playersChoice === objects[2]) {
+    if (computersChoice === objects[1]) {
+      displayResults(win + resultStatement)
+    } else {
+      displayResults(lose + resultStatement)
+    }
+  }
+}
+
+function displayResults(result) {
+  document.querySelector('#result').innerText = result;
+}
